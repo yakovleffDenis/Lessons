4dc432fa3a19ff1304c1dbdaa970d07e3128ff64
denys.yakovlev <yakovleffdenis@gmail.com>
2019-12-11 18:10:29 +0200

    sdf

---
+++ src/lessons/Break_Continue.java
@@ -0,0 +1,11 @@
+package lessons;
+
+public class Break_Continue {
+    public static void main(String[] args) {
+        int i=0;
+        while(true){
+            System.out.println(i);
+            i++;
+        }
+    }
+}
--- src/lessons/DoWhile.java
+++ src/lessons/DoWhile.java
@@ -1,7 +1,15 @@
 package lessons;
 
+import java.util.Scanner;
+
 public class DoWhile {
     public static void main(String[] args) {
-
+        Scanner scaner = new Scanner(System.in);
+        int value;
+        do{
+            System.out.println("Enter 5");
+            value = scaner.nextInt();
+        } while (value!=5);
+        System.out.println("You entered 5");
     }
 }
