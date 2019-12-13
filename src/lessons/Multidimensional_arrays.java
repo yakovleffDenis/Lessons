abdbbd199d38da83345457de96be2b3f89e0df55
denys.yakovlev <yakovleffdenis@gmail.com>
2019-12-13 12:47:22 +0200

    new

--- src/lessons/Multidimensional_arrays.java
+++ src/lessons/Multidimensional_arrays.java
@@ -2,6 +2,7 @@ package lessons;
 
 public class Multidimensional_arrays {
     public static void main(String[] args) {
-
+        int [] numbers = {1,2,3};
+        System.out.println(numbers[1]);
     }
 }
