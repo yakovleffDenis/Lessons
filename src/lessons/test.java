bc769ec74184dfa19dbc67acb9e18748c3d6598b
denys.yakovlev <yakovleffdenis@gmail.com>
2019-12-13 14:34:27 +0200

    new

--- src/lessons/test.java
+++ src/lessons/test.java
@@ -2,15 +2,14 @@ package lessons;
 
 public class test {
     public static void main(String[] args) {
-        int [][] matrice = {{1,2,3,4,5},
+        int[][] matrice = {{1, 2, 3, 4, 5},
                 {4},
-                {7,8,9,0}};
-        for (int i=0; i<matrice.length; i++) {
-            for (int j=0; j<matrice[i].length;j++){
-                System.out.println("Количество элементов в 1-строке"+ matrice[i]);
-                System.out.print(matrice[i][j] +" ");
-            }
-            System.out.println();
+                {7, 8, 9, 0}};
+        for (int i = 0; i < matrice.length; i++) {
+            //for (int j=0; j<matrice[i].length;j++){
+            //System.out.println("Количество элементов в "+ (i+1) +"-строке " + matrice[i].length);
+            System.out.println(String.format("Количество элементов в %d-строке %d", i+1, matrice[i].length));
+        //}
         }
     }
 }
