53e6b0cdaf6a124a17b1693fe95ff4f9a199ec6d
denys.yakovlev <yakovleffdenis@gmail.com>
2019-12-13 12:23:35 +0200

    new

--- src/lessons/Arrays_of_Strings.java
+++ src/lessons/Arrays_of_Strings.java
@@ -6,10 +6,10 @@ public class Arrays_of_Strings {
         num[0] = 10;
         System.out.println(num[0]); //первый =10
         System.out.println(num[1]); //первый = пустой
-        String [] text = new String[3];
-        text[0] = "Hello";
-        text[1] = "Goodbye";
-        text[2] = "Java";
+        String [] text = {"a", "b", "c"};// new String[3];
+//        text[0] = "Hello";
+//        text[1] = "Goodbye";
+//        text[2] = "Java";
         System.out.println(text[0]+" "+text[1]);
         System.out.println(text[2]);
         System.out.println();
@@ -22,5 +22,15 @@ public class Arrays_of_Strings {
         for (String x:text){
             System.out.println(x);
         }
+        int [] numbers1 = {1,2,3};
+        int sum = 0;
+        for (int x:numbers1){
+            sum=sum+x;}
+            System.out.println();
+            System.out.println(sum);
+
+            int value = 0;
+            String s = null;
+        System.out.println(s);
     }
 }
