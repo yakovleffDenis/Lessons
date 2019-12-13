2d3bfe009726aa1d86fdaad75e968b63d92825d6
denys.yakovlev <yakovleffdenis@gmail.com>
2019-12-13 13:13:46 +0200

    new

--- src/lessons/Multidimensional_arrays.java
+++ src/lessons/Multidimensional_arrays.java
@@ -2,7 +2,13 @@ package lessons;
 
 public class Multidimensional_arrays {
     public static void main(String[] args) {
-        int [] numbers = {1,2,3};
+        int[] numbers = {1, 2, 3};
         System.out.println(numbers[1]);
+        /**
+         * 123
+         * 456 5 - [1] [1]
+         * 789
+         */
+        int [][] matrice = {{1,2,3},{4,5,6},{7,8,9}};
     }
 }
