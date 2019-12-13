6a5174acfcb804dca37efb123be032c7a15be543
denys.yakovlev <yakovleffdenis@gmail.com>
2019-12-13 13:49:12 +0200

    new

--- src/lessons/Multidimensional_arrays.java
+++ src/lessons/Multidimensional_arrays.java
@@ -3,7 +3,7 @@ package lessons;
 public class Multidimensional_arrays {
     public static void main(String[] args) {
         int[] numbers = {1, 2, 3};
-        System.out.println(numbers[1]);
+        System.out.println("1 - element" + numbers[1]);
         /**
          * 123
          * 456 5 - [1] [1]
@@ -12,7 +12,22 @@ public class Multidimensional_arrays {
         int [][] matrice = {{1,2,3},
                             {4,5,6},
                             {7,8,9}};
-        System.out.println(matrice[2][2]);//9
-        System.out.println(matrice[1][0]); //4
+        int sum = matrice[2][2]+matrice[1][0];
+        System.out.println("9-element = " + matrice[2][2]);//9
+        System.out.println("4-element = " + matrice[1][0]); //4
+        System.out.println("They sum is = " + sum);//9+4
+        System.out.println();
+
+        for (int i=0; i<matrice.length; i++) {
+            for (int j=0; j<matrice[i].length;j++){
+                System.out.print(matrice[i][j] +" ");
+            }
+           System.out.println();
+        }
+        System.out.println();
+        int [] numbers1 = new int[5];
+        String [][] text = new String[2][3];
+        text [0][1] = "Hello";
+        System.out.println(text[0][1]);
     }
 }
