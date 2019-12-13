fda6cec3b857c43f003754ec10b4c5e86e39a0e0
denys.yakovlev <yakovleffdenis@gmail.com>
2019-12-13 17:43:19 +0200

    new

--- src/lessons/ClassesAndObjects.java
+++ src/lessons/ClassesAndObjects.java
@@ -3,23 +3,25 @@ package lessons;
 public class ClassesAndObjects {
     public static void main(String[] args) {
         Person person1 = new Person();
-            person1.name = "John";
+            person1.setName("John");
             person1.age = 50;
+            String s1 = "Den";
+        Person person2 = new Person();
+            person2.setName(s1);
+            person2.age = 20;
             person1.sayHello();
             person1.speak();
-
-        Person person2 = new Person();
-            person2.name = "Den";
-            person2.age = 20;
             person2.speak();
-            int year2 = person2.calculateYearsToRetirement();
-        System.out.println("Person 2 to retirement is " + year2 + " years");
     }
 }
 
 class Person {
     String name;
     int age;
+
+    void setName(String username){
+        name = username;
+    }
 
     int calculateYearsToRetirement(){
         int years = 65-age;
