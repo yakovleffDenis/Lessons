785fe77eb05fb59dd0b96289ed193c5379d41a10
denys.yakovlev <yakovleffdenis@gmail.com>
2019-12-13 15:58:35 +0200

    new

--- src/lessons/ClassesAndObjects.java
+++ src/lessons/ClassesAndObjects.java
@@ -5,15 +5,26 @@ public class ClassesAndObjects {
         Person person1 = new Person();
             person1.name = "John";
             person1.age = 30;
+            person1.sayHello();
+            person1.speak();
         Person person2 = new Person();
             person2.name = "Den";
             person2.age = 33;
-        System.out.println("My name is " + person1.name +", " + "I'm "+ person1.age + " years old");
-        System.out.println("My name is " + person2.name +", " + "I'm "+ person2.age + " years old");
+            person2.speak();
     }
 }
 
 class Person {
     String name;
     int age;
-}
+    void speak() {
+//        for (int i = 0; i < 3; i++) {
+            System.out.println("My name is" + name + "," + " I'm " + age + " years old");
+//        }
+        System.out.println();
+
+    }
+        void sayHello(){
+            System.out.print("Hello"+","+" ");
+        }
+    }
